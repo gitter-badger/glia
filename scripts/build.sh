87674f4988d271397b74ed5f1de4f8b037b7a413
Konstantin Itskov <konstantin.itskov@kovits.com>
2017-07-17 00:33:24 -0400

    Change build bash script to using python argparse

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,45 +1,29 @@
 #!/usr/bin/env bash
 
-function usage()
-{
-    echo "usage: npm run build -- [-h] [--cmake_build_type]"
-    echo ""
-    echo "options arguments:"
-    echo "  -h, --help              show this help message and exit"
-    echo "  --cmake_build_type      valid options: Debug, Release, RelWithDebInfo, MinSizeRel"
-    echo ""
-}
+CMAKE_BUILD_TYPE=
 
-for i in "$@"; do
-    case ${i} in
-        -h|--help)
-            usage
-            exit 0
-            ;;
-        --cmake_build_type)
-            CMAKE_BUILD_TYPE=${2}
-            shift
-            ;;
-        *)
-            ;;
-    esac
-done
+args=`cat << EOF | python3.6 - ${@}
+import argparse
+import os
 
-if [ -z "${CMAKE_BUILD_TYPE}" ]; then
-    echo "this should not happen"
-    CMAKE_BUILD_TYPE=Debug
-fi
+parser = argparse.ArgumentParser(
+    add_help=False,
+    formatter_class=argparse.RawTextHelpFormatter
+)
+parser.add_argument('-h', '--help', action="help")
+parser.add_argument(
+    '--cmake_build_type',
+    metavar='',
+    default='Debug',
+    choices=['Debug','Release', 'RelWithDebInfo', 'MinSizeRel'],
+    help='choose the compilation build type. [Debug]'
+)
+args = parser.parse_args()
 
-cmake_build_types=(Debug Release RelWithDebInfo MinSizeRel)
-for i in "${cmake_build_types[@]}"; do
-    if [ "${i}" == "${CMAKE_BUILD_TYPE}" ]; then
-        cmake -H. -Bbuild/${CMAKE_BUILD_TYPE}/ -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        cmake --build build/${CMAKE_BUILD_TYPE}/
-        exit 0
-    else
-        continue
-    fi
-done
+print('CMAKE_BUILD_TYPE="%s";' % args.cmake_build_type)
+EOF
+`
+eval ${args}
 
-usage
-exit 1
+cmake -H. -Bbuild/${CMAKE_BUILD_TYPE}/ -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+cmake --build build/${CMAKE_BUILD_TYPE}/
